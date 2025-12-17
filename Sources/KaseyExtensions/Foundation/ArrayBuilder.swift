f57272a1c2adefd4e1a48c647c8a2f7ed8b4d0c1
Kasey Baughan <kaseyb002@yahoo.com>
2025-12-16 21:09:43 -0500

    ArrayBuilder:

--- Sources/KaseyExtensions/Foundation/ArrayBuilder.swift
+++ Sources/KaseyExtensions/Foundation/ArrayBuilder.swift
@@ -1,22 +1,22 @@
 import Foundation
 
 @resultBuilder
-internal enum ArrayBuilder<Element> {
-    static func buildExpression(_ expression: Element) -> [Element] { [expression] }
-    static func buildExpression(_ expression: [Element]) -> [Element] { expression }
-    static func buildBlock(_ components: [Element]...) -> [Element] {
+public enum ArrayBuilder<Element> {
+    public static func buildExpression(_ expression: Element) -> [Element] { [expression] }
+    public static func buildExpression(_ expression: [Element]) -> [Element] { expression }
+    public static func buildBlock(_ components: [Element]...) -> [Element] {
         components.flatMap { $0 }
     }
-    static func buildOptional(_ component: [Element]?) -> [Element] {
+    public static func buildOptional(_ component: [Element]?) -> [Element] {
         component ?? []
     }
-    static func buildEither(first component: [Element]) -> [Element] { component }
-    static func buildEither(second component: [Element]) -> [Element] { component }
-    static func buildArray(_ components: [[Element]]) -> [Element] {
+    public static func buildEither(first component: [Element]) -> [Element] { component }
+    public static func buildEither(second component: [Element]) -> [Element] { component }
+    public static func buildArray(_ components: [[Element]]) -> [Element] {
         components.flatMap { $0 }
     }
-    static func buildLimitedAvailability(_ component: [Element]) -> [Element] { component }
-    static func buildFinalResult(_ component: [Element]) -> [Element] { component }
+    public static func buildLimitedAvailability(_ component: [Element]) -> [Element] { component }
+    public static func buildFinalResult(_ component: [Element]) -> [Element] { component }
 }
 
 extension Array {
